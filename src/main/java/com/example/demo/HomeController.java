7aa1b833479ed2d1b7593518ff2c7b67842c4e63
Tyirga <Esyan4@gmail.com>
2018-02-13 09:59:11 -0500

    update21

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,20 +1,35 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/")
-    public String index(){
-        return "index";
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value="/register", method= RequestMethod.GET)
+    public String showRegistrationPage(Model model){
+        model.addAttribute("user", new User());
+        return "registration";
     }
-@RequestMapping("/login")
-    public String login(){
-        return "login";
-}
-@RequestMapping("/secure")
-    public String secure(){
-        return "secure";
-}
+    @RequestMapping(value="/register", method=RequestMethod.POST)
+    public String processRegistrationPage(
+            @Valid @ModelAttribute("user") User user,
+            BindingResult result, Model model){
+        if(result.hasErrors()){
+            return "registration";
+        }else {
+            userService.saveUser(user);
+            model.addAttribute("message","user Account successfully created");
+        }
+      return "index";
+    }
 }
